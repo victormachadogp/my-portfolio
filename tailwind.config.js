0a8308f2783e50d1b20316485b6439d0665c794f
Victor Machado <victormachadogp@gmail.com>
2020-03-23 21:08:14 -0300

    Tailwind custom file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#fbfbfb",
+        "secondary": "#929292"
+      },
+      fontSize: {
+        "7xl" : "5.5rem",
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
