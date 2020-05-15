5d1b2fa58e6aa58837340ef1ada02a9ef20bc997
Victor Machado <victormachadogp@gmail.com>
2020-05-14 23:55:57 -0300

    ProjectSection added

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <NavBar/>
+    <ProjectSection/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import ProjectSection from './components/ProjectSection.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    ProjectSection
   }
 }
 </script>
---
+++ vue-portfolio/src/components/ProjectSection.vue
@@ -0,0 +1,45 @@
+<template>
+<section class="md:mt-32 my-12 md:mt-16">
+        <p class="text-2xl font-bold mb-8 md:m-12 md:text-left text-center">Projects</p>
+        <div class="flex md:justify-around flex-col md:flex-row mx-4 md:mx-6">
+
+            <div class="md:w-1/4 bg-gray-300 rounded-lg mt-5 md:mt-0">
+                <img class="rounded rounded-b-none p-px" src="">
+                <div class="p-5 md:h-48">
+                    <h4 class="font-bold text-lg mb-1">The Kesh Website</h4>
+                    <p class="">
+                        Non hendrerit orcipharetra nec. Quisque rutrum massa vitae ipsum vulputate, nec tristique mi placerat massa vitae ipsum vulputate, nec tristique mi placerat.
+                    </p>
+                </div>
+            </div>
+
+            <div class="md:w-1/4 bg-gray-300 rounded-lg mt-5 md:mt-0">
+                <img class="rounded rounded-b-none p-px" src="">
+                <div class="p-5 md:h-48">
+                    <h4 class="font-bold text-lg mb-1">The Kesh Website</h4>
+                    <p class="">
+                        Non hendrerit orcipharetra nec. Quisque rutrum massa vitae ipsum vulputate, nec tristique mi placerat massa vitae ipsum vulputate, nec tristique mi placerat.
+                    </p>
+                </div>
+            </div>
+
+            <div class="md:w-1/4 bg-gray-300 rounded-lg mt-5 md:mt-0">
+                <img class="rounded rounded-b-none p-px" src="">
+                <div class="p-5 md:h-48">
+                    <h4 class="font-bold text-lg mb-1">The Kesh Website</h4>
+                    <p class="">
+                        Non hendrerit orcipharetra nec. Quisque rutrum massa vitae ipsum vulputate, nec tristique mi placerat massa vitae ipsum vulputate, nec tristique mi placerat.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
