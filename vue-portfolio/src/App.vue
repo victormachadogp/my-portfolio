3c625561397a760d461db3c275da9e294fa48dc6
Victor Machado <victormachadogp@gmail.com>
2020-05-15 23:48:50 -0300

    ArticleSection added

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <NavBar/>
     <ProjectSection/>
+    <ArticleSection/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
 import ProjectSection from './components/ProjectSection.vue'
+import ArticleSection from './components/ArticleSection.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    ProjectSection
+    ProjectSection,
+    ArticleSection
   }
 }
 </script>
---
+++ vue-portfolio/src/components/ArticleSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="mb-10">
+        <p class="text-3xl font-semibold mb-4 md:m-12 md:text-left mx-5">Articles</p>
+        <div class="flex flex-col">
+
+
+            <div class=" bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
+                <div class="p-5">
+                    <h4 class="font-semibold text-xl mb-3">How to work with Vue Binding</h4>
+                    <p class="text-tiny">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ea ex nam rerum laborum blanditiis, dolorum ut. Labore esse odit tempore rem eveniet, ullam suscipit ipsum magni excepturi accusantium harum nemo id distinctio quia molestiae perferendis?
+                    </p>
+                </div>
+            </div> 
+
+            <div class=" bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
+                <div class="p-5">
+                    <h4 class="font-semibold text-xl mb-3">How to use Vue Props</h4>
+                    <p class="text-tiny">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ea ex nam rerum laborum blanditiis, dolorum ut. Labore esse odit tempore rem eveniet, ullam suscipit ipsum magni excepturi accusantium harum nemo id distinctio quia molestiae perferendis?
+                    </p>
+                </div>
+            </div>
+
+        </div>    
+
+    </section>
+</template>
