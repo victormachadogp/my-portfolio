30fb61391ce970ecac66682059103f723f20b87d
Victor Machado <victormachadogp@gmail.com>
2020-05-18 15:26:09 -0300

    NavBar design change

--- vue-portfolio/src/components/NavBar.vue
+++ vue-portfolio/src/components/NavBar.vue
@@ -1,39 +1,67 @@
 <template>
-    <section class="bg-primary">
-    <nav class="md:p-5">
-        <div class="flex flex-col md:flex-row justify-between">
-            <div class="leading-tight mb-3 md:mb-0 md:m-0 m-5">
-                <h1 class="text-3xl font-bold md:hidden">Victor Machado.</h1>
-                <h2 class="font-light ml-2 md:ml-0 text-xl md:text-2xl text-gray-500">Front-End Developer</h2>
-            </div>
-            <div class="flex justify-between md:bg-primary md:py-0 md:border-t-0 md:border-b-0 md:my-0 py-4 border-t border-b bg-white my-2">
-                <!-- <img class="w-6 mx-10 md:mx-6 cursor-pointer" src="../assets/images/github-image.svg">
-                <img class="w-6 mx-10 md:mx-6 cursor-pointer" src="../assets/images/gmail.svg">
-                <img class="w-6 mx-10 md:mx-6 cursor-pointer" src="../assets/images/linkedin.svg"> -->
-            </div>
-        </div>
-        <div class="container mx-1 my-5 bg-secondary h-px w-full hidden md:block"></div>
-    </nav>
+  <section class="">
+    <SvgHeaderOne class="absolute top-0 left-0" />
+    <SvgHeaderTwo class="absolute top-0 right-0" />
 
-    <section class="flex justify-center md:mt-16 mt-2 md:mx-10">
-        <div class="md:w-2/5 mx-5 ">
-            <h3 class="text-4xl mb-2 md:mb-0 md:text-6xl">Hello!</h3>
-            <p class="font-base md:text-lg">I’m a Front-End Developer based in São Paulo - BR. Currently focusing in Javascript and Vue</p>
-        </div>
-        <div class=" hidden md:block self-center border-secondary md:border-r md:h-32"></div>
-        <div class="hidden md:flex items-center md:pl-12 mx-auto ">
-            <h1 class="text-6xl font-bold">Victor Machado.</h1>
-        </div>
-    </section>
-    </section>
+    <div class="container-header">
+    <div class="text-center">   
+        <h2 class="text-4xl md:text-6xl">Victor <span class="md:bg-blue-500 font-semibold">Machado</span></h2>
+        <p class="text-lg">Front-end Developer</p>
+    </div>
+
+    <div class="text-center my-8">
+        <button class="text-2xl border px-16 py-1 border-black rounded-full hover:bg-black hover:text-white">Projects</button>
+    </div>
+    
+    <div class="flex justify-center">
+     <GithubIcon class="social-icon" />
+     <LinkedinIcon class="social-icon"/>
+     <EmailIcon class="social-icon"/>
+     <TwitterIcon class="social-icon"/>
+     </div>
+     </div>
+
+  </section>
 </template>
 
 <script>
+import SvgHeaderOne from "@/components/svg/SvgHeaderOne.vue";
+import SvgHeaderTwo from "@/components/svg/SvgHeaderTwo.vue";
+import GithubIcon from "@/components/icons/GithubIcon.vue";
+import LinkedinIcon from "@/components/icons/LinkedinIcon.vue";
+import EmailIcon from "@/components/icons/EmailIcon.vue";
+import TwitterIcon from "@/components/icons/TwitterIcon.vue";
 export default {
-    
-}
+  components: {
+    SvgHeaderOne,
+    SvgHeaderTwo,
+    GithubIcon,
+    LinkedinIcon,
+    EmailIcon,
+    TwitterIcon
+  }
+};
 </script>
 
 <style scoped>
+.social-icon {
+    width: 39px;
+    height: 39px;
+    margin-top: 1rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    cursor: pointer;
+}
 
+.container-header {
+    margin-top: 10rem;
+    margin-bottom: 14rem;
+}
+
+@media (max-width: 780px) {
+    .container-header {
+    margin-top: 18rem;
+}
+
+}
 </style>
