ee14ffd50d289c272ff74d0ab4e9323e2e720122
Victor Machado <victormachadogp@gmail.com>
2020-05-30 01:38:31 -0300

    Fixed alignment

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,7 +6,7 @@
         <div class="line-decoration"></div>
         </div>
 
-        <p class="mt-8">I’m a Front-End Developer based in São Paulo - BR. Currently focusing in <span class="font-bold">Javascript</span> and <span class="font-bold">Vue</span>
+        <p class="mt-8 md:text-center">I’m a Front-End Developer based in São Paulo - BR. Currently focusing in <span class="font-bold">Javascript</span> and <span class="font-bold">Vue</span>
 </p>
 
     </section>
