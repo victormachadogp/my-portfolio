2d4bb99f71d9336f878c0a95fb807fd029750763
Victor Machado <victormachadogp@gmail.com>
2020-05-30 01:11:58 -0300

    SvgHeaderOne changed

--- src/components/svg/SvgHeaderOne.vue
+++ src/components/svg/SvgHeaderOne.vue
@@ -1,19 +1,29 @@
 <template>
-<svg width="1440" height="243" viewBox="0 0 1440 243" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="-19" y="-40.4341" width="1459" height="283.434" fill="url(#paint0_linear)"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M1652.49 -42.5589L1055 -135.49L935.398 243H1562.26L1652.49 -42.5589Z" fill="url(#paint1_linear)"/>
+<svg viewBox="0 0 1440 509" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0 509H1440V-7.39587H0V509Z" fill="url(#paint0_linear)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M1012.19 -7.39591L935.398 509H1440V-7.39591H1012.19ZM1652.49 -97.8372L1652 -98.0001V-94.5215L1652.49 -97.8372Z" fill="url(#paint1_linear)"/>
 <defs>
-<linearGradient id="paint0_linear" x1="236.205" y1="38.4925" x2="1165.31" y2="531.938" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint0_linear" x1="236.205" y1="136.403" x2="1334.08" y2="456.439" gradientUnits="userSpaceOnUse">
 <stop stop-color="#0077FF"/>
 <stop offset="1" stop-color="#00C3FF"/>
 </linearGradient>
-<linearGradient id="paint1_linear" x1="1371.5" y1="223.005" x2="1057.68" y2="186.201" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint1_linear" x1="1371.5" y1="466.509" x2="1054.33" y2="449.005" gradientUnits="userSpaceOnUse">
 <stop stop-color="#00C3FF"/>
 <stop offset="1" stop-color="#0085FF"/>
 </linearGradient>
 </defs>
 </svg>
 
-
-
 </template>
+
+<style scoped>
+
+@media (min-width: 768px){
+ .test {
+  width: 120%;
+  height: 100%;
+  display: none;
+}
+}
+
+</style>
