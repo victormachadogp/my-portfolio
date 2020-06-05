45ce0aca9d5467fcb10c3db3327838a8437b9f38
Victor Machado <victormachadogp@gmail.com>
2020-06-04 23:07:44 -0300

    Old project box remvoed

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -26,28 +26,6 @@
         </div>
     </div>
 
-    <!-- Box 1 -->
-    <!-- <div class=" lg:max-w-sm lg:block container-projects hover:shadow-2xl mb-3 bg-white">
-        <div class="px-6 pt-12 pb-5 md:pt-0 md:pb-0 md:px-0 lg:pt-6 lg:pb-5 lg:px-8 flex flex-col md:flex-row lg:flex-col items-center">
-        <div class="md:m-4 lg:m-0">
-            <ComputerGradient class="md:w-24 md:h-24 image-icon" width="7rem" height="7rem"/>
-        </div>
-        <div class="mt-6 md:m-4 lg:mt-6 lg:mb-0 lg:mx-0">
-        <h3 class="my-2 flex text-2xl md:text-xl font-semibold">How Vue bind really works?</h3>
-            <p class="text-left ">
-                Vue binding is one of the most used directives that you’ll use while working with vue, and here you will learn what you can and can’t do with it
-            </p>
-        </div>
-        </div>
-        <div class="mb-5 px-6">
-        <p class="word-filter inline-block px-3 rounded mr-3 cursor-pointer">Vue</p>
-        <p class="word-filter inline-block px-3 rounded mr-3 cursor-pointer">Nuxt</p>
-        <p class="word-filter inline-block px-3 rounded cursor-pointer">Tailwind</p>
-        </div>
-    </div> -->
-
-
-
     <!-- Box 2 -->
  <div class=" lg:max-w-sm container-projects hover:shadow-2xl mb-3">
         <div class="px-6 pt-12 pb-5 md:pt-0 md:pb-0 md:px-0 lg:pt-6 lg:pb-5 lg:px-8 flex flex-col md:flex-row lg:flex-col items-center">
