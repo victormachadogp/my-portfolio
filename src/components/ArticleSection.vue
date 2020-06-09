e201b9605d1e57b6be8917bf92d07e207b43ffce
Victor Machado <victormachadogp@gmail.com>
2020-06-08 23:28:02 -0300

    Details added

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -16,17 +16,26 @@
                         <span class="mx-2 grey-color">|</span>
                         <div class="text-sm grey-color">25 MAY 2020</div>
                     </div>
+                    <div class="w-full bg-gray-400 h-px mt-8"></div>
                 </div>
+                
             </div> 
 
             <div class=" bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
                 <div class="p-5">
-                    <h4 class="font-semibold text-xl mb-3">How to use Vue Props</h4>
-                    <p class="text-tiny">
+                    <h4 class="article-title text-tiny">How to work with Vue Binding</h4>
+                    <p class="text-gray-700">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ea ex nam rerum laborum blanditiis, dolorum ut. Labore esse odit tempore rem eveniet.
                     </p>
+                    <div class="flex items-center mt-4">
+                        <div class="w-8 h-8 bg-gray-400 rounded-full mr-2 "></div>
+                        <p class="grey-color">Victor Machado</p>
+                        <span class="mx-2 grey-color">|</span>
+                        <div class="text-sm grey-color">25 MAY 2020</div>
+                    </div>
+                        <div class="w-full bg-gray-400 h-px mt-8"></div>
                 </div>
-            </div>
+            </div> 
 
         </div>    
 
