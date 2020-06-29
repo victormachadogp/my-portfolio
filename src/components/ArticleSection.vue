b9231aacb540c3b34533463e75e36fb3dc515139
Victor Machado <victormachadogp@gmail.com>
2020-06-28 23:14:19 -0300

    Typo fixed

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -2,14 +2,10 @@
   <section class="mb-10">
     <p class="text-3xl font-semibold mb-4 md:m-12 text-center mx-5">Articles</p>
     <div class="flex flex-col items-center md:ml-4">
-      <div
-        class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0"
-      >
+      <div class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
         <div class="p-5">
           <h4 class="article-title">How to use with Vue Binding</h4>
-          <p class="text-gray-700">
-            Vue binding is one of the most used features inside a Vue app.
-          </p>
+          <p class="text-gray-700">Vue binding is one of the most used features inside a Vue app</p>
           <div class="flex items-center mt-4">
             <div class="w-8 h-8 bg-gray-400 rounded-full mr-2"></div>
             <p class="grey-color">Victor Machado</p>
@@ -20,9 +16,7 @@
         </div>
       </div>
 
-      <div
-        class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0"
-      >
+      <div class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
         <div class="p-5">
           <h4 class="article-title">How to work with Vue Binding</h4>
           <p class="text-gray-700">
