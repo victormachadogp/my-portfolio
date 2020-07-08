1a55bde6d4f7be1537a0807aea3af116854edf93
Victor Machado <victormachadogp@gmail.com>
2020-07-07 23:28:45 -0300

    Content properly added

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,30 +1,42 @@
 <template>
-    <section class="px-8">
-        <h2 class="text-left md:text-center text-5xl font-semibold mb-4">Hello</h2>
+  <section class="px-8">
+    <h2 class="text-left md:text-center text-5xl font-semibold mb-4">Hello</h2>
 
-        <div class="flex justify-start md:justify-center">
-        <div class="line-decoration"></div>
-        </div>
+    <div class="flex justify-start md:justify-center">
+      <div class="line-decoration"></div>
+    </div>
 
-        <p class="mt-8 md:text-center text-lg">I’m a Front-End Developer based in São Paulo - BR. Currently focusing in <span class="font-bold">Javascript</span> and <span class="font-bold">Vue</span>
-</p>
+    <p class="mt-8 md:text-center text-lg">
+      I’m a Front-End Developer based in São Paulo - BR. Currently focusing in
+      <span
+        class="font-bold"
+      >Javascript</span> and
+      <span class="font-bold">Vue</span>
+    </p>
 
-    </section>
+    <div class="flex justify-center my-12">
+      <div class="flex bg-gray-300 text-lg rounded-full">
+        <p
+          class="px-3 py-1 border-r border-gray-500 hover:bg-gray-600 hover:border-gray-600 rounded-l-full"
+        >Projects</p>
+        <p
+          class="px-3 py-1 border-r border-gray-500 hover:bg-gray-600 hover:border-gray-600"
+        >Articles</p>
+        <p class="px-3 py-1 hover:bg-gray-600 hover:border-gray-600 rounded-r-full">Designs</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
 .line-decoration {
-    background: linear-gradient(90deg, #0077FF 4.99%, #00C3FF 88.98%);
-    border-radius: 4px;
-    width: 60px ;
-    height: 7px;
+  background: linear-gradient(90deg, #0077ff 4.99%, #00c3ff 88.98%);
+  border-radius: 4px;
+  width: 60px;
+  height: 7px;
 }
-
 </style>
