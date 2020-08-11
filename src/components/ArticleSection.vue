66fbad028acc103fae3db7eb0932c807ada3e321
Victor Machado <victormachadogp@gmail.com>
2020-08-10 22:04:38 -0300

    WIP: Fixing the max width of article container

--- src/components/ArticleSection.vue
+++ src/components/ArticleSection.vue
@@ -2,6 +2,7 @@
   <section class="mb-10">
     <p class="text-3xl font-semibold mb-4 md:m-12 text-center mx-5">Articles</p>
     <div class="flex flex-col items-center md:ml-4">
+      <!-- Article 1 -->
       <div class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
         <div class="p-5">
           <h4 class="article-title">How to use with Vue Binding</h4>
@@ -16,19 +17,19 @@
         </div>
       </div>
 
+      <!-- Article 2 -->
       <div class="bg-white shadow-custom md:mx-3 md:w-2/4 rounded-lg my-6 md:mt-0">
         <div class="p-5">
-          <h4 class="article-title">How to work with Vue Binding</h4>
+          <h4 class="article-title">How to use with Vue Binding</h4>
           <p class="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ea
-            ex nam rerum laborum blanditiis, dolorum ut. Labore esse odit
-            tempore rem eveniet.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            ex
           </p>
           <div class="flex items-center mt-4">
             <div class="w-8 h-8 bg-gray-400 rounded-full mr-2"></div>
             <p class="grey-color">Victor Machado</p>
             <span class="mx-2 grey-color">|</span>
-            <div class="text-sm grey-color">25 MAY 2020</div>
+            <div class="text-sm grey-color">25 JUNE 2020</div>
           </div>
           <div class="w-full bg-gray-400 h-px mt-6"></div>
         </div>
@@ -36,7 +37,7 @@
     </div>
   </section>
 </template>
-
+    
 <script>
 export default {};
 </script>
