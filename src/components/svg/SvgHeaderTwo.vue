2fb5690b454f0b56bed336e9a8fbaf7f45754534
Victor Machado <victormachadogp@gmail.com>
2021-05-10 16:16:36 -0300

    Class name changes

--- src/components/svg/SvgHeaderTwo.vue
+++ src/components/svg/SvgHeaderTwo.vue
@@ -1,31 +1,51 @@
 <template>
-<svg viewBox="0 0 1440 243" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="-19" y="-40.4341" width="1459" height="283.434" fill="url(#paint3_linear)"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M1652.49 -42.5589L1055 -135.49L935.398 243H1562.26L1652.49 -42.5589Z" fill="url(#paint2_linear)"/>
-<defs>
-<linearGradient id="paint3_linear" x1="236.205" y1="38.4925" x2="1165.31" y2="531.938" gradientUnits="userSpaceOnUse">
-<stop stop-color="#0077FF"/>
-<stop offset="1" stop-color="#00C3FF"/>
-</linearGradient>
-<linearGradient id="paint2_linear" x1="1371.5" y1="223.005" x2="1057.68" y2="186.201" gradientUnits="userSpaceOnUse">
-<stop stop-color="#00C3FF"/>
-<stop offset="1" stop-color="#0085FF"/>
-</linearGradient>
-</defs>
-</svg>
-
+  <svg viewBox="0 0 1440 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect
+      x="-19"
+      y="-40.4341"
+      width="1459"
+      height="283.434"
+      fill="url(#paint3_linear)"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M1652.49 -42.5589L1055 -135.49L935.398 243H1562.26L1652.49 -42.5589Z"
+      fill="url(#paint2_linear)"
+    />
+    <defs>
+      <linearGradient
+        id="paint3_linear"
+        x1="236.205"
+        y1="38.4925"
+        x2="1165.31"
+        y2="531.938"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#0077FF" />
+        <stop offset="1" stop-color="#00C3FF" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear"
+        x1="1371.5"
+        y1="223.005"
+        x2="1057.68"
+        y2="186.201"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#00C3FF" />
+        <stop offset="1" stop-color="#0085FF" />
+      </linearGradient>
+    </defs>
+  </svg>
 </template>
 
 <style scoped>
-
-@media (max-width: 767px){
- .testTwo {
-  width: 120%;
-  height: 100%;
-  display: none;
+@media (max-width: 767px) {
+  .mobile-width-fix {
+    width: 120%;
+    height: 100%;
+    display: none;
+  }
 }
-}
-
-
-
 </style>
