89ef54d8bb5a7bff89cf2d4eb7c9b59287de5f41
Victor Machado <victormachadogp@gmail.com>
2021-05-10 16:14:51 -0300

    Filter Project removed

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,30 +1,16 @@
 <template>
-  <section class="px-8">
+  <section class="px-8 mb-10">
     <h2 class="text-left md:text-center text-5xl font-semibold mb-4">Hello</h2>
 
     <div class="flex justify-start md:justify-center">
       <div class="line-decoration"></div>
     </div>
 
-    <p class="mt-8 md:text-center text-lg">
+    <p class="mt-8 md:text-center text-lg mb-16">
       I’m a Front-End Developer based in São Paulo - BR. Currently focusing in
-      <span
-        class="font-bold"
-      >Javascript</span> and
+      <span class="font-bold">Javascript</span> and
       <span class="font-bold">Vue</span>
     </p>
-
-    <div class="flex justify-center my-12">
-      <div class="flex bg-gray-300 text-lg rounded-full">
-        <p
-          class="px-3 py-1 border-r border-gray-500 hover:bg-gray-600 hover:border-gray-600 rounded-l-full"
-        >Projects</p>
-        <p
-          class="px-3 py-1 border-r border-gray-500 hover:bg-gray-600 hover:border-gray-600"
-        >Articles</p>
-        <p class="px-3 py-1 hover:bg-gray-600 hover:border-gray-600 rounded-r-full">Designs</p>
-      </div>
-    </div>
   </section>
 </template>
 
