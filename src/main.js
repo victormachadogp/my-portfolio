20dac56a21d67c393ca3581b6027e0f07471cd32
Victor Machado <victormachadogp@gmail.com>
2021-06-01 17:30:27 -0300

    Style file name change

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/styles/index.css';
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/styles/style.css";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
