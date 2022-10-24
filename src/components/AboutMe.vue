99c484c321b5aa20b113d233c37c5dbe00dfa674
Victor Machado <victormachadogp@gmail.com>
2022-10-24 13:17:41 -0300

    Removing content from all page components

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,21 +1,5 @@
 <template>
-  <section class="px-8 mb-10">
-    <h2 class="text-left md:text-center text-5xl font-semibold mb-4">Hello</h2>
-
-    <div class="flex justify-start md:justify-center">
-      <div class="line-decoration"></div>
-    </div>
-
-    <p
-      class="mt-8 md:text-center text-lg mb-16 sm:max-w-lg md:max-w-xl md:mx-auto"
-    >
-      I’m a Front-End Developer with a degree in
-      <span class="font-bold">Graphic Design.</span> Besides coding I'm a big
-      fan of art related stuff. Based in São Paulo - BR. Currently focusing on
-      <span class="font-bold">Javascript</span> and
-      <span class="font-bold">Vue</span>
-    </p>
-  </section>
+  <div></div>
 </template>
 
 <script>
@@ -23,10 +7,4 @@ export default {};
 </script>
 
 <style scoped>
-.line-decoration {
-  background: linear-gradient(90deg, #0077ff 4.99%, #00c3ff 88.98%);
-  border-radius: 4px;
-  width: 60px;
-  height: 7px;
-}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,10 @@
 <template>
-  <footer class="text-center pb-8">
-    <p class="text-xs text-gray-500 py-1">Coded and Designed by</p>
-    <h3 class="text-2xl text-gray-800">Victor Machado.</h3>
-    <div class="border-b-2 border-gray-400 my-2"></div>
-    <p class="text-xs text-gray-500">Seeking Learning.</p>
-  </footer>
+  <div></div>
 </template>
 
-<style scoped>
-.bg-color {
-  background-color: #0077ff;
-}
+<script>
+export default {};
+</script>
+
+<style>
 </style>
