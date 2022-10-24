0ea282f05a565839b98cc10ba73c3bacc9b0833f
Victor Machado <victormachadogp@gmail.com>
2022-10-24 18:43:31 -0300

    Project Section and Items added

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p class="font-bold text-lg">Todo - REST API</p>
+    <p class="mt-1.5">
+      Todo List developed using Vue and Tailwind CSS, I used json-server as a
+      fake api to handle the data.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,10 +1,41 @@
 <template>
-  <div></div>
+  <div class="flex mt-10 mb-10">
+    <div class="w-1/2 font-bold">Check out my projects</div>
+    <div class="w-1/2">
+      <div class="space-y-8">
+        <project-item />
+        <project-item />
+        <project-item />
+      </div>
+      <button
+        class="
+          uppercase
+          btn-style
+          font-bold
+          border border-black
+          rounded-full
+          px-7
+          py-3
+          mt-12
+        "
+      >
+        View All Github Projects
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import ProjectItem from "./ProjectItem.vue";
+export default {
+  components: {
+    ProjectItem,
+  },
+};
 </script>
 
 <style>
+.btn-style {
+  box-shadow: 2px 2px 0px #f1f1f1, 3px 3px 0px #222222;
+}
 </style>
