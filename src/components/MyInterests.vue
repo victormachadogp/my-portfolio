000cf7ce75804b6901f2f0f2e37d89bca060f12c
Victor Machado <victormachadogp@gmail.com>
2022-10-25 18:19:01 -0300

    My Interests component added

---
+++ src/components/MyInterests.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2 class="text-4xl font-medium">My Interests</h2>
+
+    <div class="mt-12 mb-20">
+      <div class="flex">
+        <div class="w-4/5">
+          <div>
+            <img src="../assets/space-image.jpg" alt="" />
+          </div>
+          <div class="flex mt-6">
+            <div class="w-3/5">
+              <p class="text-2xl font-medium leading-6">
+                NASA, Hubble <br />
+                Space Telescope
+              </p>
+              <p class="text-gray-500 text-sm pt-1">
+                A piece from my favorite Hubble photo
+              </p>
+            </div>
+            <p class="w-2/5 font-medium text-sm">
+              A monster young star 200,000 times brighter than our Sun that is
+              blasting powerful ultraviolet radiation and hurricane-like stellar
+              winds.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/5 underline">
+          <p>Check Original</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
