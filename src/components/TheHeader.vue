461788fad301540f4bd020907c03b4b394fd476e
Victor Machado <victormachadogp@gmail.com>
2022-10-26 14:43:04 -0300

    Information icons added inside the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,13 +7,32 @@
           Victor <span><br />Machado</span>
         </h2>
       </div>
-      <div class="media-info w-1/2 flex justify-end items-end">Media Info</div>
+      <div class="w-1/2 flex flex-col space-y-3.5 justify-end items-end">
+        <a href="https://github.com/victormachadogp" target="_blank">
+          <GithubIcon class="w-6 h-6" />
+        </a>
+        <a href="https://www.linkedin.com/in/victormachadogp/" target="_blank">
+          <LinkedinIcon class="w-6 h-6" />
+        </a>
+        <a href="https://twitter.com/victormachadogp" target="_blank">
+          <TwitterIcon class="w-6 h-6" />
+        </a>
+        <a href="mailto:victormachadogp@gmail.com">
+          <EmailIcon class="w-6 h-6" />
+        </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import EmailIcon from "./icons/EmailIcon.vue";
+import GithubIcon from "./icons/GithubIcon.vue";
+import LinkedinIcon from "./icons/LinkedinIcon.vue";
+import TwitterIcon from "./icons/TwitterIcon.vue";
+export default {
+  components: { EmailIcon, GithubIcon, LinkedinIcon, TwitterIcon },
+};
 </script>
 
 <style>
