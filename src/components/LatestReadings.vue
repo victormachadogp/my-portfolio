ea9580075bd1d6b1f92fb68c593a2b649a219e43
Victor Machado <victormachadogp@gmail.com>
2022-10-26 18:53:05 -0300

    Latest Readings hover effect added

--- src/components/LatestReadings.vue
+++ src/components/LatestReadings.vue
@@ -2,12 +2,20 @@
   <div class="flex mb-32">
     <div class="w-1/2"></div>
     <div class="w-1/2">
-      <h3 class="text-2xl font-medium border-b border-black pb-2">
-        Latest Readings
-      </h3>
+      <h3 class="text-2xl font-medium pb-2">Latest Readings</h3>
 
-      <div>
-        <div class="flex justify-between py-3 border-b border-black pb-3">
+      <div class="border-x-fix">
+        <div
+          class="
+            flex
+            justify-between
+            py-3
+            pb-3
+            text-gray-500
+            border-x-fix border-t border-b border-gray-400
+            hover:border-black hover:text-black
+          "
+        >
           <p>Crime and Punishment</p>
           <p>Fiódor Dostoiévski</p>
         </div>
@@ -18,15 +26,33 @@
           someone else's.”
         </p>
       </div>
-      <div class="">
-        <div class="flex justify-between py-3 border-b border-black pb-3">
+      <div
+        class="
+          border-t
+          text-gray-500
+          hover:text-black
+          position-top
+          border-b border-gray-400
+          hover:border-black
+        "
+      >
+        <div class="flex justify-between py-3 pb-3">
           <p>Liev Tolstói</p>
           <p>Anna Karênina</p>
         </div>
         <p class="leading-8 py-3 hidden">Quote</p>
       </div>
-      <div class="">
-        <div class="flex justify-between py-3 border-b border-black pb-3">
+      <div
+        class="
+          border-t
+          text-gray-500
+          hover:text-black
+          position-top-2
+          border-b border-gray-400
+          hover:border-black
+        "
+      >
+        <div class="flex justify-between py-3 pb-3">
           <p>Frankenstein</p>
           <p>Mary Shelley</p>
         </div>
@@ -41,4 +67,17 @@ export default {};
 </script>
 
 <style>
+.position-top {
+  position: relative;
+  bottom: 1px;
+}
+
+.position-top-2 {
+  position: relative;
+  bottom: 2px;
+}
+
+.border-x-fix:hover ~ .position-top {
+  border-top: 1px solid #000;
+}
 </style>
