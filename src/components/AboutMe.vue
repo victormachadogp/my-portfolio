efac7c491dc1da87322a82b0c38f2867e218dcdc
Victor Machado <victormachadogp@gmail.com>
2022-10-27 16:26:56 -0300

    About Me reponsive in sm screens

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <div class="flex py-28 items-center">
-      <div class="align-start text-5xl w-1/2">
-        <h2 class="flex w-36 items-end flex-nowrap flex-col font-medium">
+  <div class="px-2.5 sm:px-0">
+    <div class="flex flex-col sm:flex-row py-28 sm:items-center items-start">
+      <div class="align-start text-5xl sm:w-1/2 w-full">
+        <h2
+          class="
+            flex
+            w-36
+            sm:items-end
+            items-start
+            flex-nowrap flex-col
+            font-medium
+            sm:pb-0
+            pb-12
+          "
+        >
           <p class="">About</p>
           <p class="">Me</p>
         </h2>
       </div>
-      <div class="w-1/2 max-w-xl text-xl leading-8">
+      <div class="sm:w-1/2 w-full max-w-xl text-xl leading-8">
         I’m a Front-End Developer with a degree in Graphic Design. Besides
         coding I'm a big fan of art related stuff. Based in São Paulo - BR.
         Currently focusing on Javascript and Vue.
