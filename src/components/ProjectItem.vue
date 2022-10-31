8f436b8d8e2d8264a6fa4295c762d8983929d761
Victor Machado <victormachadogp@gmail.com>
2022-10-31 11:09:08 -0300

    Logic to add the banner added

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <div class="flex items-center">
-      <p class="font-bold text-lg">Todo - REST API</p>
-      <img class="ml-2" src="../assets/arrow-angle.svg" alt="" />
+    <div class="project-info pb-3">
+      <div class="flex items-center">
+        <p class="font-bold text-lg">Todo - REST API</p>
+        <img class="ml-2" src="../assets/arrow-angle.svg" alt="" />
+      </div>
+      <p class="mt-1.5">
+        Todo List developed using Vue and Tailwind CSS, I used json-server as a
+        fake api to handle the data.
+      </p>
     </div>
-    <p class="mt-1.5">
-      Todo List developed using Vue and Tailwind CSS, I used json-server as a
-      fake api to handle the data.
-    </p>
+    <div class="image-container">
+      <!-- <img class="w-full" src="../assets/site-banner-1.jpg" alt="" /> -->
+    </div>
   </div>
 </template>
 
@@ -15,5 +20,22 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+.project-info ~ .image-container {
+  width: 100%;
+  height: 0px;
+  transition-duration: 0.35s;
+}
+
+.project-info:hover ~ .image-container {
+  height: 250px;
+  transition-duration: 0.35s;
+  background-color: green;
+}
+
+.image-container:hover {
+  height: 250px;
+  transition-duration: 0.35s;
+  background-color: green;
+}
 </style>
