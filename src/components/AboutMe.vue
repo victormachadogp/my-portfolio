a39b7a3eb4eb9ec926c4a3a3a6f2957da5b48695
Victor Machado <victormachadogp@gmail.com>
2022-11-01 17:44:57 -0300

    See if this change affect original url

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,13 +7,20 @@
     </div>
 
     <p
-      class="mt-8 md:text-center text-lg mb-16 sm:max-w-lg md:max-w-xl md:mx-auto"
+      class="
+        mt-8
+        md:text-center
+        text-lg
+        mb-16
+        sm:max-w-lg
+        md:max-w-xl md:mx-auto
+      "
     >
       I’m a Front-End Developer with a degree in
       <span class="font-bold">Graphic Design.</span> Besides coding I'm a big
       fan of art related stuff. Based in São Paulo - BR. Currently focusing on
-      <span class="font-bold">Javascript</span> and
-      <span class="font-bold">Vue</span>
+      <span class="font-bold">Javascript</span>,
+      <span class="font-bold">Vue</span> and <span class="font-bold">Nuxt</span>
     </p>
   </section>
 </template>
